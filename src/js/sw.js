31f7ef86d2692c4bf8064b99cebc51c69965a269
Gehaime Barros <{ID}+{gehaimeBarros}@users.noreply.github.com>
2024-07-08 15:58:17 -0300

    DEV0089489384938891092RBR

--- src/js/sw.js
+++ src/js/sw.js
@@ -2,10 +2,36 @@ const CACHE_NAME = 'sheetfrenzy-v1';
 const urlsToCache = [
     '/',
     './index.html',
-    '/src/css/styles.css',
-    '/src/js/app.js',
-    '/src/img/logoPage200.png',
-    '/src/img/logoPage.png'
+    './src/css/styles.css',
+    './src/js/scriptI.js',
+    './src/js/scriptFunc.js',
+    './favicon.ico',
+    './src/img/200.png',
+    './src/img/cb1-PhotoRoom.png',
+    './src/img/cb2-PhotoRoom.png',
+    './src/img/ccumber.png',
+    './src/img/docs.png',
+    './src/img/excel.png',
+    './src/img/fav.png',
+    './src/img/file.png',
+    './src/img/footer1.png',
+    './src/img/gherkin-b.png',
+    './src/img/logoPage.png',
+    './src/img/logoPage200.png',
+    './src/img/play.png',
+    './src/img/remove-folder.gif',
+    './src/img/remove.png',
+    './src/img/Sky.jpg',
+    './src/img/slogo1.png',
+    './src/img/slogo2.png',
+    './src/img/uuui.jpg',
+    './src/js/app.js',
+    './src/js/charts.js',
+    './src/js/contact.js',
+    './src/js/scriptBlog.js',
+    './src/js/scriptGerador.js',
+    './src/js/scriptSwift.js',
+    './src/js/sw.js'
 ];
 
 // Instalando o Service Worker e cacheando os recursos
@@ -14,7 +40,21 @@ self.addEventListener('install', function (event) {
         caches.open(CACHE_NAME)
             .then(function (cache) {
                 console.log('Cache aberto');
-                return cache.addAll(urlsToCache);
+                return Promise.all(
+                    urlsToCache.map(url => {
+                        return fetch(url).then(response => {
+                            if (!response.ok) {
+                                throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+                            }
+                            return cache.put(url, response);
+                        }).catch(error => {
+                            console.error(`Falha ao adicionar ${url} ao cache:`, error);
+                        });
+                    })
+                );
+            })
+            .catch(function (error) {
+                console.error('Falha ao abrir o cache:', error);
             })
     );
 });
